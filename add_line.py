641d317c79caa6fe46477dcf5d3e511cdfdfa363
Morvan Zhou <mz@email.com>
2017-09-28 22:18:44 +1000

    left menu

--- add_line.py
+++ add_line.py
@@ -1,17 +1,1 @@
-import os
-
-def add_line(path):
-    for root, dirs, files in os.walk(path):
-        print(root, dirs, files)
-        for i, f_name in enumerate(files):
-            if ".md" in f_name:
-                file_path = root+'/'+f_name
-                with open(file_path, 'r') as f:
-                    contents = f.readlines()
-                    # add content here
-                    contents.insert(1, 'index: %s\n' % str(i+1))
-                with open(file_path, 'w') as f:
-                    contents = "".join(contents)
-                    f.write(contents)
-
-add_line('_contents/tutorials/machine-learning/test')
+# TDO: set published back
