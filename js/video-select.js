c08c77be3d7e7fad2ccb86947143b997aab3dfde
Morvan Zhou <mz@email.com>
2017-10-13 09:48:32 +1100

    video

--- js/video-select.js
+++ js/video-select.js
@@ -26,5 +26,5 @@ function chooseVideo(src)
 {
     var video = '<iframe id="myVideo" class="myvideo" width="560" height="315" src=' + src + ' frameborder="0" allowfullscreen></iframe>';
     $("#embededvideo").remove();
-    $("#videochinaip").append(video);
+    $("#videogfw").append(video);
 }
