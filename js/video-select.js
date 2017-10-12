d33f1d351c70acc0aad74bf1409f80957bf5e9b4
Morvan Zhou <mz@email.com>
2017-10-13 08:42:20 +1100

    video

---
+++ js/video-select.js
@@ -0,0 +1,30 @@
+function videoGFW(src) {
+    if (window.XMLHttpRequest)
+    {// code for IE7+, Firefox, Chrome, Opera, Safari
+        var request = new XMLHttpRequest();
+    }
+    else
+    {// code for IE6, IE5
+        var request = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    request.open('GET', 'http://freegeoip.net/xml');
+    request.onreadystatechange = function() {
+        if (request.readyState == 4 && request.status == 200) {
+            var xmlDoc = request.responseXML;
+            var root = xmlDoc.documentElement;
+            var element = root.getElementsByTagName("CountryName");
+            var country = element[0].firstChild.nodeValue;
+            if (country != "China"){
+                chooseVideo(src);
+            }
+        }
+    }
+    request.send(null);
+}
+
+function chooseVideo(src)
+{
+    var video = '<iframe id="myVideo" class="myvideo" width="560" height="315" src=' + src + ' frameborder="0" allowfullscreen></iframe>';
+    $("#embededvideo").remove();
+    $("#videogfw").append(video);
+}
