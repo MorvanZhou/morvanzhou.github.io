a84046e01357e83defa9952c5754e321d9b3d47c
Morvan Zhou <morvanzhou@gmail.com>
2018-07-05 00:31:13 +0800

    tmp without ip filtering

--- js/video-select.js
+++ js/video-select.js
@@ -38,15 +38,15 @@ function chooseVideo(src, src_name, vid)
           var words = vid.split("&page=");
           var vid = words[0] +"/?p=" + words[1];
       }
+      if (vai) {
+          vai.style.display = "block";
+          vai.innerHTML = '(Bilibili 无法播放? 请 <a href="https://www.bilibili.com/video/av' + vid +'" target="_blank" >点击这里</a> 跳转至B站内)';
+      }
       if (src.includes("hdslb")) {
         velem.style.paddingBottom = "70%";
-        vai.style.display = "block";
-        vai.innerHTML = '(Bilibili 无法播放? 请 <a href="https://www.bilibili.com/video/av' + vid + '" target="_blank" >点击这里</a> 跳转至B站内)';
       }
       else if (src.includes("bilibili")) {
         velem.style.paddingBottom = "72.5%";
-        vai.style.display = "block";
-        vai.innerHTML = '(Bilibili 无法播放? 请 <a href="https://www.bilibili.com/video/av' + vid +'" target="_blank" >点击这里</a> 跳转至B站内)';
       }
     }
     // youku
@@ -54,9 +54,11 @@ function chooseVideo(src, src_name, vid)
       velem.style.paddingBottom = "56.25%";
       velem.style.lineHeight = "0";
       velem.style.fontSize = "0";
-      vai.style.display = "block";
       var aid = src.split("embed/")[1];
-      vai.innerHTML = '(优酷无法播放? 请 <a href="http://v.youku.com/v_show/id_' + vid +'==.html" target="_blank" >点击这里</a> 跳转至优酷站内)';
+      if (vai) {
+        vai.style.display = "block";
+        vai.innerHTML = '(优酷无法播放? 请 <a href="http://v.youku.com/v_show/id_' + vid +'==.html" target="_blank" >点击这里</a> 跳转至优酷站内)';
+      }
     }
 
     else {  // youtube
